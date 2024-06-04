d2471fddfc280248c89b866ca29c3c2032c73d23
sadhakunps <sadaqunps@gmail.com>
2024-06-04 20:22:08 +0530

    learing java

---
+++ duplicate.js
@@ -0,0 +1,9 @@
+let a=(10,10,10)
+let emp={
+    id:101,
+    nam:"nove",
+    nam:"tata",
+    nam:"rgv"
+}
+console.log(a)
+console.log(emp)
---
+++ forof.js
@@ -0,0 +1,16 @@
+// easy way to iterate indexing
+
+let eids=[10,20,30,60,30,709,62,75,]
+let sizes=['s','y','t','e','b','n']
+let name=["tata", "infosis","congizent","tcs"]
+console.log(eids[0])
+
+    for(eid of eids){
+        console.log(eid)
+    }
+
+    for(size of sizes){
+        console.log(size)
+    }
+
+
